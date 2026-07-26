33a6335b4196af0dfef99531cfb5be829e731e4f
koji-osa <junjunior22@gmail.com>
2026-07-26 07:20:33 +0000

    chore: v1.2.14リリース準備(バージョン番号更新)
    Obsidianノート一覧の並び順改善(更新日時降順)・URL省略機能の
    Obsidianタブ適用を含むv1.2.14としてバージョン番号を更新。

--- readaloud_app/lib/main.dart
+++ readaloud_app/lib/main.dart
@@ -31,7 +31,7 @@ void main() async {
   await AudioService.androidForceEnableMediaButtons();
 
   // FIX-021調査用ログ初期化
-  await DebugLogger.instance.init(appVersion: '1.2.13+34');
+  await DebugLogger.instance.init(appVersion: '1.2.14+35');
 
   runApp(
     ProviderScope(
