2308434b84723c7b12120a80bc60e3cbc229fc22
koji-osa <junjunior22@gmail.com>
2026-07-30 20:26:44 +0000

    chore: v1.2.19リリース準備(バージョン番号更新)
    Obsidianノート選択画面に「日付で探す」モードを追加した
    v1.2.19としてバージョン番号を更新。

--- readaloud_app/lib/main.dart
+++ readaloud_app/lib/main.dart
@@ -31,7 +31,7 @@ void main() async {
   await AudioService.androidForceEnableMediaButtons();
 
   // FIX-021調査用ログ初期化
-  await DebugLogger.instance.init(appVersion: '1.2.18+39');
+  await DebugLogger.instance.init(appVersion: '1.2.19+40');
 
   runApp(
     ProviderScope(
